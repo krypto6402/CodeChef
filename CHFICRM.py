57138bb0919858f844a58e63d4baad0cf58606ee
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-06-19 07:17:35 +0530

    copied....

--- CHFICRM.py
+++ CHFICRM.py
@@ -1,23 +1,83 @@
 # cook your dish here
-t = int(input())
-while (t):
-    t = t-1
-    n = int(input())
-    a = list(map(int,input().split()))
-    bal = 0
-    c = 0
-    if a[0] != 5:
-        print('NO')
-    if a[0] == 5:
-        bal = 5
-        c = c+1
-        for i in range (1,n):
-            if bal-(a[i]-5) >= 0:
-                c = c+1
-                bal = bal+5
-            else:
-                break
-        if c == n:
-            print('YES')
-        else:
-            print('NO')
+
+# Python program to check whether X can  
+# give change to every person in the Queue 
+  
+# Function to check if every person  
+# will get the change from X 
+def isChangeable(notes, n): 
+      
+    # To count the 5$ and 10& notes 
+    fiveCount = 0
+    tenCount = 0
+      
+    # Serve the customer in order 
+    for i in range(n): 
+          
+        # Increase the number of 5$ note by one 
+        if (notes[i] == 5): 
+            fiveCount += 1
+        elif(notes[i] == 10): 
+              
+            # decrease the number of note 5$  
+            # and increase 10$ note by one 
+            if (fiveCount > 0): 
+                fiveCount -= 1
+                tenCount += 1
+            else: 
+                return 0
+        else: 
+              
+            # decrease 5$ and 10$ note by one 
+            if ( tenCount > 0): 
+                tenCount -= 1
+                  
+            # decrease 5$ note by three 
+            elif (fiveCount >= 2): 
+                fiveCount -= 2
+            else: 
+                return 0
+    return 1
+# Driver Code 
+
+# queue of customers with available notes. 
+for _ in range(int(input())):
+    
+    n = int(input()) 
+    a=[int(a) for a in input().split()]
+    
+    # Calling function 
+    if (isChangeable(a, n)): 
+     print("YES") 
+    else: 
+     print("NO") 
+    
+    # This code is contributed by PrinciRaj1992
+    
+    
+    
+    
+    
+#   SELF (WRONG ANSWER)
+'''
+t=int(input())
+for ti in range(t):
+     n=int(input())
+     a=list(map(int,input().split()))
+     earnedmoney=0
+     check=1
+     for i in range(len(a)):
+          if(a[i]==5):
+               earnedmoney+=5
+               continue
+          returningmoney=a[i]-5
+          if(returningmoney > earnedmoney):
+               flag=0
+               break
+          else:
+               earnedmoney = earnedmoney - returningmoney + 5
+     if(check==0):
+          print("NO")
+     elif(check==1):
+          print("YES")
+'''
